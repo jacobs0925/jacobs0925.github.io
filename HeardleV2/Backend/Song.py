7cc28a25c38b9a4f4b6c770762680e5d6f8669d6
jacobs0925 <jjschwar@uci.edu>
2023-06-13 00:41:42 -0700

    m

--- HeardleV2/Backend/Song.py
+++ HeardleV2/Backend/Song.py
@@ -70,5 +70,5 @@ def getAlbumArtLinks(artist_name, song_name):
     else:
         print("Song not found.")
         
-print(getAlbumArtLinks('Charli XCX', 'Boom Clap'))
-Song('https://youtu.be/ek1f0Sb7v70', 'test','test')
+print(getAlbumArtLinks('SZA', 'Love Galore'))
+Song('https://youtu.be/YJV-4IyNUfA', 'test','test')
